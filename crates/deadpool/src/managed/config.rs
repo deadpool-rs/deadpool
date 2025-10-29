deadpool-rs/deadpool

commit 9426cce8703161885eb90e0eb77a8b68c4a30420

Fix default pool max size

crates/deadpool/src/managed/config.rs
@@ -48,8 +48,8 @@ impl PoolConfig {
 }
 
 impl Default for PoolConfig {
-    /// Creates a new [`PoolConfig`] with the `max_size` being set to
-    /// `cpu_count * 4` ignoring any logical CPUs (Hyper-Threading).
+    /// Create a [`PoolConfig`] where [`PoolConfig::max_size`] is set to
+    /// `cpu_core_count * 2` including logical cores (Hyper-Threading).
     fn default() -> Self {
         Self::new(crate::util::get_default_pool_max_size())
     }

crates/deadpool/src/unmanaged/config.rs
@@ -33,7 +33,7 @@ impl PoolConfig {
 
 impl Default for PoolConfig {
     /// Create a [`PoolConfig`] where [`PoolConfig::max_size`] is set to
-    /// `cpu_count * 4` ignoring any logical CPUs (Hyper-Threading).
+    /// `cpu_core_count * 2` including logical cores (Hyper-Threading).
     fn default() -> Self {
         Self::new(crate::util::get_default_pool_max_size())
     }

crates/deadpool/src/util.rs
@@ -1,16 +1,12 @@
 lazy_static::lazy_static! {
-    /// Cache the physical CPU count to avoid calling `num_cpus::get_physical`
-    /// multiple times, which is expensive.
-    ///
-    /// TODO: in container environment, we should use the cpus limit of the container
-    /// instead of the number of physical CPUs. Thus, we should call `num_cpus::get()`
-    /// instead of `num_cpus::get_physical()`, which is aware of cgroup. This may be
-    /// a breaking change so we don't do it for now.
-    static ref PHYSICAL_CPU_COUNT: usize = num_cpus::get_physical();
+    /// Cache the physical CPU count to avoid calling `num_cpus::get()`
+    /// multiple times, which is expensive when creating pools in quick
+    /// succession.
+    static ref CPU_COUNT: usize = num_cpus::get();
 }
 
-/// Get the default maximum size of a pool, which is `cpu_count * 4` ignoring
-/// any logical CPUs (Hyper-Threading).
+/// Get the default maximum size of a pool, which is `cpu_core_count * 2`
+/// including logical cores (Hyper-Threading).
 pub(crate) fn get_default_pool_max_size() -> usize {
-    *PHYSICAL_CPU_COUNT * 4
+    *CPU_COUNT * 2
 }

commit 53d65f55cde2b50b0a4a79d1c6272b0e9c3bd610

Fix documented max_size default.
Fixes: 9426cce("Fix default pool max size")

crates/deadpool/src/managed/config.rs
@@ -10,7 +10,7 @@ use super::BuildError;
 pub struct PoolConfig {
     /// Maximum size of the [`Pool`].
     ///
-    /// Default: `cpu_count * 4`
+    /// Default: `cpu_core_count * 2`
     ///
     /// [`Pool`]: super::Pool
     pub max_size: usize,